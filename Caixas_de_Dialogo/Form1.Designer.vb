632526dbfb4e75771496796c63221d671ef16452
jmbda <jmbda@SHOVE_THEKING>
2024-01-12 01:21:41 +0000

    Add project files.

---
+++ Caixas_de_Dialogo/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FicheiroToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NovoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuardarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SairToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormatarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TipoDeLetraToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FicheiroToolStripMenuItem, Me.FormatarToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FicheiroToolStripMenuItem
+        '
+        Me.FicheiroToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NovoToolStripMenuItem, Me.ToolStripSeparator1, Me.AbrirToolStripMenuItem, Me.GuardarToolStripMenuItem, Me.ToolStripSeparator2, Me.SairToolStripMenuItem})
+        Me.FicheiroToolStripMenuItem.Name = "FicheiroToolStripMenuItem"
+        Me.FicheiroToolStripMenuItem.Size = New System.Drawing.Size(75, 24)
+        Me.FicheiroToolStripMenuItem.Text = "Ficheiro"
+        '
+        'NovoToolStripMenuItem
+        '
+        Me.NovoToolStripMenuItem.Name = "NovoToolStripMenuItem"
+        Me.NovoToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.NovoToolStripMenuItem.Text = "Novo"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(221, 6)
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.AbrirToolStripMenuItem.Text = "&Abrir..."
+        '
+        'GuardarToolStripMenuItem
+        '
+        Me.GuardarToolStripMenuItem.Name = "GuardarToolStripMenuItem"
+        Me.GuardarToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.GuardarToolStripMenuItem.Text = "&Guardar..."
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(221, 6)
+        '
+        'SairToolStripMenuItem
+        '
+        Me.SairToolStripMenuItem.Name = "SairToolStripMenuItem"
+        Me.SairToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.SairToolStripMenuItem.Text = "&Sair"
+        '
+        'FormatarToolStripMenuItem
+        '
+        Me.FormatarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CorToolStripMenuItem, Me.TipoDeLetraToolStripMenuItem})
+        Me.FormatarToolStripMenuItem.Name = "FormatarToolStripMenuItem"
+        Me.FormatarToolStripMenuItem.Size = New System.Drawing.Size(83, 24)
+        Me.FormatarToolStripMenuItem.Text = "Formatar"
+        '
+        'CorToolStripMenuItem
+        '
+        Me.CorToolStripMenuItem.Name = "CorToolStripMenuItem"
+        Me.CorToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.CorToolStripMenuItem.Text = "Cor..."
+        '
+        'TipoDeLetraToolStripMenuItem
+        '
+        Me.TipoDeLetraToolStripMenuItem.Name = "TipoDeLetraToolStripMenuItem"
+        Me.TipoDeLetraToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.TipoDeLetraToolStripMenuItem.Text = "Tipo de Letra"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 28)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(800, 422)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Bloco de Notas"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents FicheiroToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NovoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GuardarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents SairToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TipoDeLetraToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Caixas_de_Dialogo/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub AbrirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AbrirToolStripMenuItem.Click
+        Me.OpenFileDialog1.Title = "Abrir..."
+        Me.OpenFileDialog1.FileName = ""
+        Me.OpenFileDialog1.Filter = "Ficheiros de texto(*.txt) | *.txt | Todos (*.*) | *.*"
+        Me.OpenFileDialog1.FilterIndex = 1
+        If Me.OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Me.TextBox1.Text = File.ReadAllText(Me.OpenFileDialog1.FileName)
+            Me.TextBox1.Tag = Nothing
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox1.Tag = "Alterado"
+    End Sub
+
+    Private Sub NovoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NovoToolStripMenuItem.Click
+        If TextBox1.Tag IsNot Nothing Then
+            Dim res As DialogResult
+            res = MessageBox.Show("O documento foi alterado. Deseja continuar?", "Editor de Notas", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning)
+            If res <> Windows.Forms.DialogResult.Yes Then
+                Return
+            End If
+        End If
+        Me.TextBox1.Text = String.Empty
+        Me.TextBox1.Tag = Nothing
+    End Sub
+
+    Private Sub GuardarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GuardarToolStripMenuItem.Click
+        Me.SaveFileDialog1.Title = "Guardar..."
+        Me.SaveFileDialog1.DefaultExt = "txt"
+        Me.SaveFileDialog1.Filter = "Ficheiros de texto (*.txt) | *.txt"
+        Me.SaveFileDialog1.DefaultExt = "txt"
+
+        If Me.SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            File.WriteAllText(Me.SaveFileDialog1.FileName, Me.TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub SairToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SairToolStripMenuItem.Click
+        If TextBox1.Tag IsNot Nothing Then
+            Dim res As DialogResult
+            res = MessageBox.Show("O documento foi alterado. Deseja continuar?", "Editor de Notas", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning)
+            If res <> Windows.Forms.DialogResult.Yes Then
+                Return
+            End If
+        End If
+        Application.Exit()
+    End Sub
+
+    Private Sub FormatarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FormatarToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub CorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CorToolStripMenuItem.Click
+        Me.ColorDialog1.Color = TextBox1.ForeColor
+
+        If Me.ColorDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Me.TextBox1.ForeColor = Me.ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub TipoDeLetraToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TipoDeLetraToolStripMenuItem.Click
+        Me.FontDialog1.ShowColor = True
+        Me.FontDialog1.Color = TextBox1.ForeColor
+
+        If Me.FontDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Me.TextBox1.Font = Me.FontDialog1.Font
+            Me.TextBox1.ForeColor = Me.FontDialog1.Color
+        End If
+    End Sub
+End Class
